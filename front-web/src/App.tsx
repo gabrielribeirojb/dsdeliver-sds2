f144b7a4a9151ccb340bc0980787a78c7681728e
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-10 23:45:24 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
-import Footer from './Footer';
-import NavBar from './Navbar';
 import Routes from './Routes';
+import { ToastContainer } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+      <>
         < Routes />
+        <ToastContainer />  
+      </>
   )
 }
 
