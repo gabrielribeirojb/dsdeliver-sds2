2d1e33dd8f761b67d36e354b5ac060854d3e2366
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-18 01:50:47 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -5,6 +5,7 @@ import Header from './src/Header';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
 import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -17,8 +18,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      < Header />
-      < Home />
+
+    < Routes />
+
       <StatusBar style="auto" />
     </View>
   );
